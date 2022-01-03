c08df6055a71e162200393cbc420671cf9721940
Juan Martin Salice <juan.salice@globant.com>
2022-01-03 18:21:50 -0300

    New script to control 3D planar layers

---
+++ unity-renderer/Assets/Rendering/ProceduralSkybox/SkyboxPlane3DManager.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxPlane3DManager : MonoBehaviour
+{
+    public List<GameObject> objectsToSpawn = new List<GameObject>();
+    public float frequency;
+    float currentTime;
+    public float speed;
+    public float distance;
+
+    public float width;
+
+    List<GameObject> spawned = new List<GameObject>();
+
+    void Start()
+    {
+        currentTime = frequency;
+    }
+
+    void Update()
+    {
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+        }
+        else
+        {
+            SpawnObject();
+            currentTime = frequency;
+        }
+
+        for (int i = 0; i < spawned.Count; i++)
+        {
+            if(spawned[i])
+            {
+                MoveObject(spawned[i]);
+            }
+        }
+    }
+
+    void SpawnObject()
+    {
+        int objIndex = SetRandomIndex();
+        Vector3 pos = SetRandomPosition() + transform.position;
+
+
+        GameObject newObj = Instantiate(objectsToSpawn[objIndex], pos, transform.rotation, transform);
+        spawned.Add(newObj);
+    }
+
+    int SetRandomIndex()
+    {
+        return Random.Range(0, objectsToSpawn.Count);
+    }
+
+    Vector3 SetRandomPosition()
+    {
+        float randomOffset = Random.Range(-width/2, width/2);
+
+        Vector3 offset = transform.right * randomOffset;
+        return offset;
+    }
+
+    void MoveObject(GameObject obj)
+    {
+        obj.transform.position += transform.forward * speed * Time.deltaTime;
+
+
+        if(Mathf.Abs(obj.transform.position.z - (transform.forward * distance).z) <= 0.1f)
+        {
+            DestroyObject(obj);
+        }
+    }
+
+    void DestroyObject(GameObject obj)
+    {
+        spawned.Remove(obj);
+        Destroy(obj);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        Vector3 lowLeft = (transform.right * -width / 2) + transform.position;
+        Vector3 lowRight = (transform.right * width / 2) + transform.position;
+
+
+        Vector3 highLeft = (transform.forward * distance) + lowLeft;
+        Vector3 highRight = (transform.forward * distance) + lowRight;
+
+        Gizmos.DrawLine(lowLeft, lowRight);
+        Gizmos.DrawLine(highLeft, highRight);
+
+        Gizmos.DrawLine(lowLeft, highLeft);
+        Gizmos.DrawLine(lowRight, highRight);
+    }
+}
