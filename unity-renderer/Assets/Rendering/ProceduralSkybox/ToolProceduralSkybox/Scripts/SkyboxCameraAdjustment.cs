972cf6bb6d734dfb17766464075532ca0fccf513
nishant-kaushik <98018257+dcl-glb@users.noreply.github.com>
2022-02-11 14:43:24 +0530

    try: camera stack setup

--- unity-renderer/Assets/Rendering/ProceduralSkybox/ToolProceduralSkybox/Scripts/SkyboxCameraAdjustment.cs
+++ unity-renderer/Assets/Rendering/ProceduralSkybox/ToolProceduralSkybox/Scripts/SkyboxCameraAdjustment.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class SkyboxCameraAdjustment : MonoBehaviour
 {
@@ -22,9 +23,14 @@ public class SkyboxCameraAdjustment : MonoBehaviour
     void AdjustMainCamera()
     {
         Camera mainCam = Camera.main;
+        var cameraData = mainCam.GetUniversalAdditionalCameraData();
+        cameraData.renderType = CameraRenderType.Overlay;
+
+        var skyBoxCamData = skyboxCamera.GetUniversalAdditionalCameraData();
+        skyBoxCamData.cameraStack.Add(mainCam);
 
         //mainCam.cullingMask = mainCam.cullingMask & ~(1 << skyboxLayer);
-        mainCam.clearFlags = CameraClearFlags.Nothing;
+        //mainCam.clearFlags = CameraClearFlags.Nothing;
         initialized = true;
     }
 }
